b35693df28974e706d6fcf583c057df92dda5f41
GPCracker <GPCracker@mail.ru>
2016-11-06 14:38:46 +0300

    Keyboard shortcut handling will check parent module status. Similar blocks aligned.

--- source/hooks/AvatarInputHandler.py
+++ source/hooks/AvatarInputHandler.py
@@ -17,73 +17,78 @@ def new_AvatarInputHandler_handleKeyEvent(self, event):
 	getShortcut = XModLib.KeyBoard.Shortcut.fromSequence
 	## HotKeys - Common
 	if self.ctrlModeName in ['arcade', 'sniper', 'strategic']:
-		## HotKeys - TargetScanner - AutoScan
-		config = _config_['commonAS']['targetScanner']['autoScan']
-		shortcutHandle = config['enabled'] and getShortcut(
-			config['shortcut']['key'],
-			config['shortcut']['switch'],
-			config['shortcut']['invert']
-		)(event)
-		if shortcutHandle and (not shortcutHandle.switch or shortcutHandle.pushed):
-			config['activated'] = shortcutHandle(config['activated'])
-			if shortcutHandle.switch and config['activated']:
-				XModLib.Messages.Messenger.showMessageOnPanel(
-					'Player',
-					None,
-					config['message']['onActivate'],
-					'green'
-				)
-			elif shortcutHandle.switch:
-				XModLib.Messages.Messenger.showMessageOnPanel(
-					'Player',
-					None,
-					config['message']['onDeactivate'],
-					'red'
-				)
-			targetScanner = getattr(self, 'XTargetScanner', None)
-			if targetScanner is not None:
-				targetScanner.autoScanActivated = config['activated']
-		## HotKeys - TargetScanner - ManualOverride
-		config = _config_['commonAS']['targetScanner']['manualOverride']
-		shortcutHandle = config['enabled'] and getShortcut(config['key'], True, False)(event)
-		if shortcutHandle:
-			if shortcutHandle.pushed:
+		## HotKeys - TargetScanner
+		config = _config_['commonAS']['targetScanner']
+		if config['enabled']:
+			## HotKeys - TargetScanner - AutoScan
+			config = _config_['commonAS']['targetScanner']['autoScan']
+			shortcutHandle = config['enabled'] and getShortcut(
+				config['shortcut']['key'],
+				config['shortcut']['switch'],
+				config['shortcut']['invert']
+			)(event)
+			if shortcutHandle and (not shortcutHandle.switch or shortcutHandle.pushed):
+				config['activated'] = shortcutHandle(config['activated'])
+				if shortcutHandle.switch and config['activated']:
+					XModLib.Messages.Messenger.showMessageOnPanel(
+						'Player',
+						None,
+						config['message']['onActivate'],
+						'green'
+					)
+				elif shortcutHandle.switch:
+					XModLib.Messages.Messenger.showMessageOnPanel(
+						'Player',
+						None,
+						config['message']['onDeactivate'],
+						'red'
+					)
+				targetScanner = getattr(self, 'XTargetScanner', None)
+				if targetScanner is not None:
+					targetScanner.autoScanActivated = config['activated']
+			## HotKeys - TargetScanner - ManualOverride
+			config = _config_['commonAS']['targetScanner']['manualOverride']
+			shortcutHandle = config['enabled'] and getShortcut(config['key'], True, False)(event)
+			if shortcutHandle and shortcutHandle.pushed:
 				targetScanner = getattr(self, 'XTargetScanner', None)
 				if targetScanner is not None:
 					targetScanner.engageManualOverride()
 	## HotKeys - Arcade
 	if self.ctrlModeName == 'arcade':
-		## HotKeys - AimCorrection - ManualMode
-		config = _config_['arcadeAS']['aimCorrection']['manualMode']
-		shortcutHandle = config['enabled'] and getShortcut(config['key'], False, False)(event)
-		if shortcutHandle:
-			self.ctrl.XAimCorrection.resetManualInfo()
-			if shortcutHandle.pushed:
-				self.ctrl.XAimCorrection.setManualInfo()
-		## HotKeys - AimCorrection - Target Mode - Switch
-		config = _config_['arcadeAS']['aimCorrection']['targetMode']
-		shortcutHandle = config['enabled'] and getShortcut(
-			config['shortcut']['key'],
-			config['shortcut']['switch'],
-			config['shortcut']['invert']
-		)(event)
-		if shortcutHandle and (not shortcutHandle.switch or shortcutHandle.pushed):
-			config['activated'] = shortcutHandle(config['activated'])
-			if shortcutHandle.switch and config['activated']:
-				XModLib.Messages.Messenger.showMessageOnPanel(
-					'Player',
-					None,
-					config['message']['onActivate'],
-					'green'
-				)
-			elif shortcutHandle.switch:
-				XModLib.Messages.Messenger.showMessageOnPanel(
-					'Player',
-					None,
-					config['message']['onDeactivate'],
-					'red'
-				)
-			self.ctrl.XAimCorrection.targetEnabled = config['activated']
+		## HotKeys - AimCorrection
+		config = _config_['arcadeAS']['aimCorrection']
+		if True:
+			## HotKeys - AimCorrection - ManualMode
+			config = _config_['arcadeAS']['aimCorrection']['manualMode']
+			shortcutHandle = config['enabled'] and getShortcut(config['key'], False, False)(event)
+			if shortcutHandle:
+				self.ctrl.XAimCorrection.resetManualInfo()
+				if shortcutHandle.pushed:
+					self.ctrl.XAimCorrection.setManualInfo()
+			## HotKeys - AimCorrection - Target Mode
+			config = _config_['arcadeAS']['aimCorrection']['targetMode']
+			shortcutHandle = config['enabled'] and getShortcut(
+				config['shortcut']['key'],
+				config['shortcut']['switch'],
+				config['shortcut']['invert']
+			)(event)
+			if shortcutHandle and (not shortcutHandle.switch or shortcutHandle.pushed):
+				config['activated'] = shortcutHandle(config['activated'])
+				if shortcutHandle.switch and config['activated']:
+					XModLib.Messages.Messenger.showMessageOnPanel(
+						'Player',
+						None,
+						config['message']['onActivate'],
+						'green'
+					)
+				elif shortcutHandle.switch:
+					XModLib.Messages.Messenger.showMessageOnPanel(
+						'Player',
+						None,
+						config['message']['onDeactivate'],
+						'red'
+					)
+				self.ctrl.XAimCorrection.targetEnabled = config['activated']
 		## HotKeys - SPG Sniper Mode
 		config = _config_['commonAS']['sniperModeSPG']
 		shortcutHandle = config['enabled'] and getShortcut(config['key'], True, False)(event)
@@ -99,37 +104,40 @@ def new_AvatarInputHandler_handleKeyEvent(self, event):
 				)
 	## HotKeys - Sniper
 	elif self.ctrlModeName == 'sniper':
-		## HotKeys - AimCorrection - ManualMode
-		config = _config_['sniperAS']['aimCorrection']['manualMode']
-		shortcutHandle = config['enabled'] and getShortcut(config['key'], False, False)(event)
-		if shortcutHandle:
-			self.ctrl.XAimCorrection.resetManualInfo()
-			if shortcutHandle.pushed:
-				self.ctrl.XAimCorrection.setManualInfo()
-		## HotKeys - AimCorrection - Target Mode - Switch
-		config = _config_['sniperAS']['aimCorrection']['targetMode']
-		shortcutHandle = config['enabled'] and getShortcut(
-			config['shortcut']['key'],
-			config['shortcut']['switch'],
-			config['shortcut']['invert']
-		)(event)
-		if shortcutHandle and (not shortcutHandle.switch or shortcutHandle.pushed):
-			config['activated'] = shortcutHandle(config['activated'])
-			if shortcutHandle.switch and config['activated']:
-				XModLib.Messages.Messenger.showMessageOnPanel(
-					'Player',
-					None,
-					config['message']['onActivate'],
-					'green'
-				)
-			elif shortcutHandle.switch:
-				XModLib.Messages.Messenger.showMessageOnPanel(
-					'Player',
-					None,
-					config['message']['onDeactivate'],
-					'red'
-				)
-			self.ctrl.XAimCorrection.targetEnabled = config['activated']
+		## HotKeys - AimCorrection
+		config = _config_['sniperAS']['aimCorrection']
+		if True:
+			## HotKeys - AimCorrection - ManualMode
+			config = _config_['sniperAS']['aimCorrection']['manualMode']
+			shortcutHandle = config['enabled'] and getShortcut(config['key'], False, False)(event)
+			if shortcutHandle:
+				self.ctrl.XAimCorrection.resetManualInfo()
+				if shortcutHandle.pushed:
+					self.ctrl.XAimCorrection.setManualInfo()
+			## HotKeys - AimCorrection - Target Mode
+			config = _config_['sniperAS']['aimCorrection']['targetMode']
+			shortcutHandle = config['enabled'] and getShortcut(
+				config['shortcut']['key'],
+				config['shortcut']['switch'],
+				config['shortcut']['invert']
+			)(event)
+			if shortcutHandle and (not shortcutHandle.switch or shortcutHandle.pushed):
+				config['activated'] = shortcutHandle(config['activated'])
+				if shortcutHandle.switch and config['activated']:
+					XModLib.Messages.Messenger.showMessageOnPanel(
+						'Player',
+						None,
+						config['message']['onActivate'],
+						'green'
+					)
+				elif shortcutHandle.switch:
+					XModLib.Messages.Messenger.showMessageOnPanel(
+						'Player',
+						None,
+						config['message']['onDeactivate'],
+						'red'
+					)
+				self.ctrl.XAimCorrection.targetEnabled = config['activated']
 		## HotKeys - SPG Sniper Mode
 		config = _config_['commonAS']['sniperModeSPG']
 		shortcutHandle = config['enabled'] and getShortcut(config['key'], True, False)(event)
@@ -146,35 +154,38 @@ def new_AvatarInputHandler_handleKeyEvent(self, event):
 				)
 	## HotKeys - Strategic
 	elif self.ctrlModeName == 'strategic':
-		## HotKeys - AimCorrection - ManualMode
-		config = _config_['strategicAS']['aimCorrection']['manualMode']
-		shortcutHandle = config['enabled'] and getShortcut(config['key'], False, False)(event)
-		if shortcutHandle:
-			self.ctrl.XAimCorrection.resetManualInfo()
-			if shortcutHandle.pushed:
-				self.ctrl.XAimCorrection.setManualInfo()
-		## HotKeys - AimCorrection - Target Mode - Switch
-		config = _config_['strategicAS']['aimCorrection']['targetMode']
-		shortcutHandle = config['enabled'] and getShortcut(
-			config['shortcut']['key'],
-			config['shortcut']['switch'],
-			config['shortcut']['invert']
-		)(event)
-		if shortcutHandle and (not shortcutHandle.switch or shortcutHandle.pushed):
-			config['activated'] = shortcutHandle(config['activated'])
-			if shortcutHandle.switch and config['activated']:
-				XModLib.Messages.Messenger.showMessageOnPanel(
-					'Player',
-					None,
-					config['message']['onActivate'],
-					'green'
-				)
-			elif shortcutHandle.switch:
-				XModLib.Messages.Messenger.showMessageOnPanel(
-					'Player',
-					None,
-					config['message']['onDeactivate'],
-					'red'
-				)
-			self.ctrl.XAimCorrection.targetEnabled = config['activated']
+		## HotKeys - AimCorrection
+		config = _config_['strategicAS']['aimCorrection']
+		if True:
+			## HotKeys - AimCorrection - ManualMode
+			config = _config_['strategicAS']['aimCorrection']['manualMode']
+			shortcutHandle = config['enabled'] and getShortcut(config['key'], False, False)(event)
+			if shortcutHandle:
+				self.ctrl.XAimCorrection.resetManualInfo()
+				if shortcutHandle.pushed:
+					self.ctrl.XAimCorrection.setManualInfo()
+			## HotKeys - AimCorrection - Target Mode
+			config = _config_['strategicAS']['aimCorrection']['targetMode']
+			shortcutHandle = config['enabled'] and getShortcut(
+				config['shortcut']['key'],
+				config['shortcut']['switch'],
+				config['shortcut']['invert']
+			)(event)
+			if shortcutHandle and (not shortcutHandle.switch or shortcutHandle.pushed):
+				config['activated'] = shortcutHandle(config['activated'])
+				if shortcutHandle.switch and config['activated']:
+					XModLib.Messages.Messenger.showMessageOnPanel(
+						'Player',
+						None,
+						config['message']['onActivate'],
+						'green'
+					)
+				elif shortcutHandle.switch:
+					XModLib.Messages.Messenger.showMessageOnPanel(
+						'Player',
+						None,
+						config['message']['onDeactivate'],
+						'red'
+					)
+				self.ctrl.XAimCorrection.targetEnabled = config['activated']
 	return
