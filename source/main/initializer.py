67a009e8a05b786ddea43b08559063d9a5f6f302
GPCracker <GPCracker@mail.ru>
2017-01-30 16:38:13 +0800

    Hook injection chain class moved to XModLib.

--- source/main/initializer.py
+++ source/main/initializer.py
@@ -7,14 +7,15 @@ def initApplication():
 	_globals_['macrosFormatter'] = XModLib.TextUtils.MacrosFormatter()
 	global _config_
 	_config_ = readConfig()
+	global _inject_chain_
+	_inject_chain_ += _inject_loads_
 	if not _config_['ignoreClientVersion'] and False:
 		print '[{0[1]}] {0[0]} is incompatible with current client version.'.format(__application__)
 		_globals_['appResultMessage'] = _config_['appFailedMessage']
 	elif _config_['applicationEnabled']:
 		print '[{0[1]}] {0[0]} successfully loaded.'.format(__application__)
 		_globals_['appResultMessage'] = _config_['appLoadedMessage']
-		_inject_chain_.append(_inject_hooks_)
-	_inject_chain_.insert(0, _inject_loads_)
+		_inject_chain_ += _inject_hooks_
 	_inject_inits_()
 	return
 
--- source/main/injector.py
+++ source/main/injector.py
@@ -1,14 +1,7 @@
 # *************************
-# Hooks injector chain
-# *************************
-class InjectorChain(list):
-	def __call__(self, *args, **kwargs):
-		return [injector(*args, **kwargs) for injector in self]
-
-# *************************
 # Hooks injector events
 # *************************
+_inject_chain_ = XModLib.HookUtils.HookChain()
 _inject_inits_ = XModLib.HookUtils.HookEvent()
 _inject_loads_ = XModLib.HookUtils.HookEvent()
 _inject_hooks_ = XModLib.HookUtils.HookEvent()
-_inject_chain_ = InjectorChain()
