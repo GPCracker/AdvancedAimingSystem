7880a846c37d9c70f5cdbdbe1b90fedfb8dfa7ef
GPCracker <GPCracker@mail.ru>
2017-02-07 20:38:11 +0800

    AutoAimExtendedTargetScan plugin bugs fixed.

--- source/plugins/AutoAimExtendedTargetScan.py
+++ source/plugins/AutoAimExtendedTargetScan.py
@@ -27,11 +27,17 @@ import XModLib.HookUtils
 # *************************
 # AutoAimControlMode Hooks
 # *************************
+@XModLib.HookUtils.methodHookExt(_inject_hooks_, CommandMapping.CommandMapping, 'isFired', invoke=XModLib.HookUtils.HookInvoke.MASTER)
+def new_CommandMapping_isFired(old_CommandMapping_isFired, self, command, key):
+	return command != CommandMapping.CMD_CM_LOCK_TARGET and old_CommandMapping_isFired(self, command, key)
+
 @XModLib.HookUtils.methodHookExt(_inject_hooks_, AvatarInputHandler.control_modes.ArcadeControlMode, 'handleKeyEvent', invoke=XModLib.HookUtils.HookInvoke.MASTER)
 @XModLib.HookUtils.methodHookExt(_inject_hooks_, AvatarInputHandler.control_modes.SniperControlMode, 'handleKeyEvent', invoke=XModLib.HookUtils.HookInvoke.MASTER)
 def new_AutoAimControlMode_handleKeyEvent(old_AutoAimControlMode_handleKeyEvent, self, isDown, key, mods, event=None):
-	if CommandMapping.g_instance.isFired(CommandMapping.CMD_CM_LOCK_TARGET, key) and isDown:
+	result = old_AutoAimControlMode_handleKeyEvent(self, isDown, key, mods, event)
+	if not result and CommandMapping.g_instance.get('CMD_CM_LOCK_TARGET') == key and isDown:
 		target = BigWorld.target()
+		# Target substitution begins.
 		if target is None and _config_['commonAS']['autoAim']['useTargetScan']:
 			targetScanner = getattr(self._aih, 'XTargetScanner', None)
 			if targetScanner is not None:
@@ -40,9 +46,6 @@ def new_AutoAimControlMode_handleKeyEvent(old_AutoAimControlMode_handleKeyEvent,
 			targetInfo = getattr(self._aih, 'XTargetInfo', None)
 			if targetInfo is not None and not targetInfo.isExpired:
 				target = targetInfo.getVehicle()
+		# Target substitution ends.
 		BigWorld.player().autoAim(target)
-		return True
-	if CommandMapping.g_instance.isFired(CommandMapping.CMD_CM_LOCK_TARGET_OFF, key) and isDown:
-		BigWorld.player().autoAim(None)
-		return True
-	return old_AutoAimControlMode_handleKeyEvent(self, isDown, key, mods, event)
+	return result
